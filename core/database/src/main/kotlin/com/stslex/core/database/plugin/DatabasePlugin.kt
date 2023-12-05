130e38ab72371a9329fd6fefa9e7b9b93ef5a3eb
stslex <ilya977.077@gmail.com>
2023-12-05 09:12:49 +0300

    Create test-deploy.yml

--- core/database/src/main/kotlin/com/stslex/core/database/plugin/DatabasePlugin.kt
+++ core/database/src/main/kotlin/com/stslex/core/database/plugin/DatabasePlugin.kt
@@ -18,8 +18,6 @@ object DatabasePlugin {
             password = Config.postgresPassword
         )
         transaction(dbPostgres) {
-            // TODO remove this line
-            SchemaUtils.dropDatabase(db.url)
             SchemaUtils.create(UserEntitiesTable)
         }
     }
